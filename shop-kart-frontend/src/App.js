33ec3f3f2b4fa01593c23f0dc7df8fa00232c99d
Nishikant Bawache <nishikant.bawache.nb@gmail.com>
2022-10-16 12:46:13 +0530

    first commit

---
+++ shop-kart-frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">shopkart</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
