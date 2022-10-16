e89a287fcf85aea9d1da757df6df858b1230f28d
Nishikant Bawache <nishikant.bawache.nb@gmail.com>
2022-10-16 13:03:38 +0530

    added data.js

---
+++ shop-kart-frontend/src/data.js
@@ -0,0 +1,52 @@
+const data = {
+    products: [
+        {
+            name: 'Nike slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality, nice shirt.'
+        },
+        {
+            name: 'Adidas slim shirt',
+            slug: 'adidas-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality, nice shirt.'
+        },
+        {
+            name: 'Nike slim pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality, nice pant.'
+        },
+        {
+            name: 'Adidas slim pant',
+            slug: 'Adidas-slim-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality, nice pant.'
+        }
+    ]
+}
