938e601b26cb9d5422a69b31a6da526ca341a99a
Nishikant Bawache <nishikant.bawache.nb@gmail.com>
2022-10-17 14:46:46 +0530

    added react-bootstrap and react-router-bootstrap

--- shop-kart-frontend/src/App.js
+++ shop-kart-frontend/src/App.js
@@ -1,21 +1,35 @@
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap'
 
 function App() {
   return (
 
     <Router>
-      <div>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to="/">shopkart</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>shopkart</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All Rights Reserved</div>
+        </footer>
       </div>
     </Router>
   );
